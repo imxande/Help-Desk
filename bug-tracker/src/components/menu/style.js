6a57f2119aab273a072a676a6e945e605a3b01b3
imxande <alerodriguezlarosa@gmail.com>
2022-01-10 17:43:37 -0500

    added more icons

--- bug-tracker/src/components/menu/style.js
+++ bug-tracker/src/components/menu/style.js
@@ -3,16 +3,31 @@ import styled from "styled-components";
 export const Container = styled.div`
   width: 4vw;
   height: 100vh;
-  border: 1px solid red;
   text-align: center;
 `;
-
+export const MenuContainer = styled.div`
+  border: 1px solid red;
+  height: 60%;
+  display: flex;
+  flex-direction: column ;
+  justify-content: space-evenly;
+  align-items: center;
+  
+`
 export const Icon = styled.i`
   font-size: 40px;
+  margin: 20px;
   color: ${(props) => props.color};
 `;
 
 export const Header = styled.header`
-  font-size: 45px;
-  margin: 20px 0px;
+  font-size: 40px;
 `;
+
+export const Footer = styled.div`
+  position: absolute; 
+  text-align: center;
+  bottom:20px;
+`;
+
+
