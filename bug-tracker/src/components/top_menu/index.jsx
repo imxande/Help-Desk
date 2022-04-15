125937dfc14a4f510698f5663b59e3cd87e4b324
imxande <alerodriguezlarosa@gmail.com>
2022-04-14 23:00:28 -0400

    added content to top menu

--- bug-tracker/src/components/top_menu/index.jsx
+++ bug-tracker/src/components/top_menu/index.jsx
@@ -1,10 +1,46 @@
 import React from "react";
-import { Container } from "./style";
+import { Container, LeftContent, RightContent } from "./style";
+import {AiOutlineSearch} from "react-icons/ai"
+import {WiDaySunnyOvercast} from "react-icons/wi"
+import {BiUserCircle} from "react-icons/bi"
 
 const TopMenu = () => {
     return (
         <div>
-            <Container/>
+            <Container>
+                <LeftContent>
+                    <ul>
+                        <li>
+                            <a href="#"> Tickets</a>
+                        </li>
+                    </ul>
+                </LeftContent>
+                <RightContent>
+                    <ul>
+                        <li>
+                            <button>
+                                <i>
+                                    <AiOutlineSearch/>
+                                </i>
+                            </button>
+                        </li>
+                        <li>
+                            <button>
+                                <i>
+                                    <WiDaySunnyOvercast/>
+                                </i>
+                            </button>
+                        </li>
+                        <li>
+                            <button>
+                                <i>
+                                    <BiUserCircle/>
+                                </i>
+                            </button>
+                        </li>
+                    </ul>
+                </RightContent>
+            </Container>
         </div>
     )
 }
--- bug-tracker/src/components/top_menu/style.js
+++ bug-tracker/src/components/top_menu/style.js
@@ -3,5 +3,13 @@ import styled from "styled-components";
 export const Container = styled.div`
   border: 1px solid red;
   width: 100%;
-  height: 10px;
+  height: 50px;
+`;
+
+export const LeftContent = styled.div`
+  border: 1px solid red;
+`;
+
+export const RightContent = styled.div`
+  border: 1px solid red;
 `;
