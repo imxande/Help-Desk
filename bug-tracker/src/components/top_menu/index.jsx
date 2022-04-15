acea31aac963f0657a2801cd20e632be15088a23
imxande <alerodriguezlarosa@gmail.com>
2022-04-14 22:09:12 -0400

    added TopMenu component

---
+++ bug-tracker/src/components/top_menu/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container } from "./style";
+
+const TopMenu = () => {
+    return (
+        <div>
+            <Container/>
+        </div>
+    )
+}
+
+export default TopMenu;
