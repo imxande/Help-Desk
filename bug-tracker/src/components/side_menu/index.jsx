e5f4b209a635477ecb74db7cb9029ea896c019b1
imxande <alerodriguezlarosa@gmail.com>
2022-04-17 21:04:10 -0400

    added corresponding text to side menu icons

--- bug-tracker/src/components/side_menu/index.jsx
+++ bug-tracker/src/components/side_menu/index.jsx
@@ -1,9 +1,8 @@
 import React from "react";
-import { Container, Icon, MenuContainer, Button, List } from "./style";
+import { Container, Icon, MenuContainer, Button, List, Text } from "./style";
 import { MdGridView, MdOutlineRssFeed, MdViewInAr } from "react-icons/md";
 import { GoCalendar } from "react-icons/go";
 import { BsBoxArrowInRight, BsTags } from "react-icons/bs";
-
 
 export const SideMenu = () => {
   return (
@@ -14,27 +13,32 @@ export const SideMenu = () => {
             <Icon color={"#483c32"}>
               <MdGridView />
             </Icon>
+            <Text>HQ</Text>
           </List>
           <List>
             <Icon color={"#483c32"}>
               <MdOutlineRssFeed />
             </Icon>
+            <Text>Feeds</Text>
           </List>
           <List>
             <Icon color={"#483c32"}>
               <MdViewInAr />
             </Icon>
+            <Text>Views</Text>
           </List>
           <List>
             <Icon color={"#483c32"}>
               <GoCalendar />
             </Icon>
+            <Text>Calendar</Text>
           </List>
-          
+
           <List>
             <Icon color={"#483c32"}>
               <BsTags />
             </Icon>
+            <Text>Tags</Text>
           </List>
         </ul>
       </MenuContainer>
