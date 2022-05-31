692a8c2436a66d238c241d52009c8836f42ef7fb
imxande <alerodriguezlarosa@gmail.com>
2022-05-31 02:41:05 -0400

    initial commit for login component

---
+++ bug-tracker/src/components/login/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Login = () => {
+    return (
+        <div>
+            1- This component needs to be a login form <br/>
+            2- Once customer login successful we should redirect to the customer dashboard
+        </div>
+    )
+}
+
+export default Login;
