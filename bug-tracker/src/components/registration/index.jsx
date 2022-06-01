0b8b0c4ee0d0e326a2839585e859bee426878f17
imxande <alerodriguezlarosa@gmail.com>
2022-05-31 22:22:43 -0400

    added registration header and form container

--- bug-tracker/src/components/registration/index.jsx
+++ bug-tracker/src/components/registration/index.jsx
@@ -1,11 +1,26 @@
 import React from "react";
+import { RegistrationPage, Container, Header, Icon, HeadingOne, FormContainer } from "./style";
+import {BsFillBugFill} from "react-icons/bs"
+
 
 const Registration = () => {
-    return (
-        <div>
-            Registration wired
-        </div>
-    )
-}
+	return (
+		<RegistrationPage>
+			<Container>
+                <Header>
+                    <Icon>
+                        <BsFillBugFill/>
+                    </Icon>
+                    <HeadingOne>
+                        Create Your Account
+                    </HeadingOne>
+                </Header>
+                <FormContainer>
+                    Lets Go
+                </FormContainer>
+            </Container>
+		</RegistrationPage>
+	);
+};
 
 export default Registration;
---
+++ bug-tracker/src/components/registration/style.js
@@ -0,0 +1,35 @@
+import styled from "styled-components";
+
+export const RegistrationPage = styled.div`
+	width: 100%;
+	height: 100vh;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+`;
+
+export const Container = styled.div`
+	width: 500px;
+	height: 700px;
+	border: 1px solid red;
+`;
+
+export const Header = styled.header`
+	border: 1px solid red;
+	text-align: center;
+	padding: 40px 40px 24px;
+`;
+
+export const Icon = styled.i`
+	font-size: 50px;
+	color: #34c7d5;
+`;
+
+export const HeadingOne = styled.h1`
+	font-size: 30px;
+	margin: 20px;
+`;
+
+export const FormContainer = styled.div`
+	padding: 0 40px 40px;
+`;
