0bb6736e36551bf0d5438983ead018e0e0302cd2
imxande <alerodriguezlarosa@gmail.com>
2022-06-02 12:16:43 -0400

    a customer is able to login

--- bug-tracker/src/components/login_form/index.jsx
+++ bug-tracker/src/components/login_form/index.jsx
@@ -1,40 +1,88 @@
-import React from "react";
-import { Container, Form, Label, Field, ForgotButton, ContinueButton, Text, SignUpButton, SignUpContent } from "./style";
-import {Link} from "react-router-dom";
+import React, { useState } from "react";
+import {
+	Container,
+	Form,
+	Label,
+	Field,
+	ForgotButton,
+	ContinueButton,
+	Text,
+	SignUpButton,
+	SignUpContent,
+} from "./style";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
 const LoginForm = () => {
-    return (
-        <Container>
-            <Form>
-                <Label htmlFor="email">
-                    <Field
-                        placeholder="Email address"
+	// our form state
+	const [authentication, setAuthentication] = useState({
+		email: "",
+		password: "",
+	});
+
+	// handle the changes in the form
+    const handleChange = (e) => {
+        // grab changes
+        const {name, value} = e.target;
+
+        // let update our state
+        setAuthentication({
+            ...authentication, 
+            [name]: value
+        })
+    } 
+	// handle submit
+	const handleSubmit = (e) => {
+		e.preventDefault();
+
+		axios
+			.post("https://ale-bug-tracker.herokuapp.com/api/auth/customers/login", authentication)
+			.then((response) => {
+				console.log(response.data);
+                // grab token
+                const {token} = response.data;
+                // store token 
+                localStorage.setItem("token", token);
+
+                // and redirect to the users dashboard
+			})
+			.catch((error) => error);
+	};
+
+	return (
+		<Container>
+			<Form onSubmit={handleSubmit}>
+				<Label htmlFor="email">
+					<Field
+						name="email"
+						type="email"
+						placeholder="Email address"
+						value={authentication.email}
+						onChange={handleChange}
+					/>
+				</Label>
+				<Label htmlFor="password">
+					<Field 
+                        name="password"
+						type="password"
+						placeholder="Password"
+						value={authentication.password}
+						onChange={handleChange}
                     />
-                </Label>
-                <Label htmlFor="password">
-                    <Field
-                        placeholder="Password"
-                    />
-                </Label>
-                <ForgotButton>
-                    Forgot password?
-                </ForgotButton>
-                <ContinueButton>
+				</Label>
+				<ForgotButton>Forgot password?</ForgotButton>
+				<ContinueButton disabled={!authentication.email || !authentication.password}>
                     Continue
                 </ContinueButton>
-                <SignUpContent>
-                    <Text>
-                        Do you have an account?
-                    </Text>
-                    <SignUpButton>
-                        <Link to="/registration">
-                            Sign Up
-                        </Link>
-                    </SignUpButton>
-                </SignUpContent>
-            </Form>
-        </Container>
-    )
-}
+				<SignUpContent>
+					<Text>Do you have an account?</Text>
+					<SignUpButton>
+						<Link to="/registration">Sign Up</Link>
+					</SignUpButton>
+				</SignUpContent>
+			</Form>
+		</Container>
+	);
+};
 
 export default LoginForm;
