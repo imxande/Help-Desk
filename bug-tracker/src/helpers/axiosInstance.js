80b82fab620ad8a33ac9121a499e7ab7bc8c7d0d
imxande <alerodriguezlarosa@gmail.com>
2022-06-02 09:28:29 -0400

    created axios instance

---
+++ bug-tracker/src/helpers/axiosInstance.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+// our base url
+const base_url = "https://ale-bug-tracker.herokuapp.com/";
+
+// create axios instance
+export default axios.create({
+	baseURL: base_url,
+	headers: {
+		"Accept-Language": "en-US",
+	},
+});
