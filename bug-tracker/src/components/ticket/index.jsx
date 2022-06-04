07254b3a57097d0f10dc2ae41c33c02bbc161dc7
imxande <alerodriguezlarosa@gmail.com>
2022-06-04 11:12:54 -0400

    added initial styles to ticket component

--- bug-tracker/src/components/ticket/index.jsx
+++ bug-tracker/src/components/ticket/index.jsx
@@ -1,20 +1,31 @@
 import React from "react";
 import TicketForm from "../ticket_form";
-import {Container,  Header, HeaderContainer, HeadingOne } from "../ticket/style";
+import { IoTicketOutline } from "react-icons/io5";
+import {
+	Container,
+	Header,
+	HeaderContainer,
+	Icon,
+	HeadingOne,
+	FormContainer,
+} from "../ticket/style";
 
 const Ticket = () => {
-    return (
-        <Container>
-            <HeaderContainer>
-                <Header>
-                    <HeadingOne>
-                        Ticket form wired!
-                    </HeadingOne>
-                </Header>
-            </HeaderContainer>
-            <TicketForm />
-        </Container>
-    )
-}
+	return (
+		<Container>
+			<HeaderContainer>
+				<Header>
+					<Icon>
+						<IoTicketOutline />
+					</Icon>
+					<HeadingOne>Create ticket</HeadingOne>
+				</Header>
+			</HeaderContainer>
+			<FormContainer>
+				<TicketForm />
+			</FormContainer>
+		</Container>
+	);
+};
 
 export default Ticket;
--- bug-tracker/src/components/ticket/style.js
+++ bug-tracker/src/components/ticket/style.js
@@ -1,11 +1,25 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+	border: 1px solid red;
+	width: 100%;
+	height: 100%;
+	display: flex;
+	flex-direction: column;
+`;
 
 export const HeaderContainer = styled.div``;
 
-export const Header = styled.header``;
+export const Header = styled.header`
+	font-size: 50px;
+	display: flex;
+	margin: 20px;
+`;
 
-export const HeadingOne = styled.h1``;
+export const Icon = styled.i``;
+
+export const HeadingOne = styled.h1`
+	margin: 0 20px;
+`;
 
 export const FormContainer = styled.div``;
