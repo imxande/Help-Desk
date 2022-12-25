5a1976b43506ccfe9db6f78cf19d099bf7ebaf23
imxande <alerodriguezlarosa@gmail.com>
2022-12-25 01:42:37 -0500

    updated dependencies, added unique id

--- bug-tracker/src/components/ticket_label/index.jsx
+++ bug-tracker/src/components/ticket_label/index.jsx
@@ -1,16 +1,26 @@
 import React from "react";
 import { Container, Subject, Priority } from "./style";
 import { setStatus } from "../../helpers/setStatus";
+import { v4 as uuidv4 } from 'uuid';
 
 const TicketLabel = ( props ) =>
 {
     // console.log( props );
     const statusColor = setStatus( props.status );
 
+    // create unique id
+    const uniqueId = uuidv4();
+
+    // priorities click handler
+    const priorityClick = ( e ) =>
+    {
+        console.log( e.target );
+    };
+
 
     return (
         <Container>
-            <Priority backgroundColor={ statusColor } />
+            <Priority id={ uniqueId } onClick={ priorityClick } backgroundColor={ statusColor } />
             <Subject>
                 { props.subject }
             </Subject>
